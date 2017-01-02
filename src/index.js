f5fb08f14106c8b8d1984cb4dd4329695801cc67
Sigurd Fosseng <sigurd@fosseng.net>
2017-01-02 16:13:31 +0100

    warn about missing targets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 // @flow
 /* eslint-disable no-console */
+import chalk from 'chalk';
 import createWatcher from './watch';
 import loadWatches from './load-watches';
 import hasWatchman from './utils/has-watchman';
 import type { WatchDefinition } from './load-watches';
 import getConfig from './config';
+
+
+type Targets = {
+  [target: string]: Array<{wd: string, data: Array<WatchDefinition> }>
+};
+const getTargets = (definitions): Targets => {
+  const targets = {};
+  definitions.forEach((def) => {
+    Object.keys(def.data).forEach((target) => {
+      targets[target] = (targets[target] || []);
+      targets[target].push({ wd: def.wd, data: def.data[target] });
+    });
+  });
+  return targets;
+};
 
 const setupPhaseWatch = (definitions, watcher, config) => (phase) => {
   definitions.forEach(({ wd, data: phaseData }) => {
@@ -33,22 +49,19 @@ const setupWatches = async (phase: string|Array<string>) => {
 
   phases.forEach(setup);
 
+  const targets = getTargets(definitions);
+  phases.forEach((p) => {
+    if (!Object.keys(targets).includes(p)) {
+      console.error(`\n${chalk.yellow('WARNING')}: target ${chalk.yellow(p)} not found in any .watch files\n`);
+    }
+  });
+
   watcher.start();
 };
 
-type Targets = {
-  [target: string]: Array<{wd: string, data: Array<WatchDefinition> }>
-};
 export const listTargets = async (): Promise<Targets> => {
   const definitions = await loadWatches();
-  const targets = {};
-  definitions.forEach((def) => {
-    Object.keys(def.data).forEach((target) => {
-      targets[target] = (targets[target] || []);
-      targets[target].push({ wd: def.wd, data: def.data[target] });
-    });
-  });
-  return targets;
+  return getTargets(definitions);
 };
 
 export default setupWatches;
